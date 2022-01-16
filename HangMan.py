13eb98ee7c8b899adb5a2407a3bec307b65aabdd
Michael Jones <mike.ecinwa@gmail.com>
2022-01-16 15:25:41 -0600

    First commit

---
+++ HangMan.py
@@ -0,0 +1,51 @@
+# Hangman
+
+import random
+
+def hangman (word):
+    wrong = 0
+    stages = ["",
+              "_________       ",
+              "|               ",
+              "|        |      ",
+              "|        O      ",
+              "|       /|\     ",
+              "|       / \     ",
+              "|               "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+
+    win = False
+    print ("Welcome to Hangman")
+    
+    while wrong < len(stages) - 1:
+        print ("\n")
+        msg = "Guess a letter "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0: wrong]))
+        print("You lose! It was {}.".format(word))
+
+def GetWord (l):
+    word = l[random.randint(0, len(l)-1)]
+    return word
+
+HangWords = ["tex", "cat", "formula", "dog", "toby"]    
+
+hangman(GetWord(HangWords))
+    
